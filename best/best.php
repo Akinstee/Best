98d5f9f0e3e0a1746687ca58fae213922f2044fc
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-20 08:49:58 +0100

    setup dashboard plugin menu

--- best/best.php
+++ best/best.php
@@ -20,7 +20,50 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
+
+
+ // If this file is access directly, abort!!!
+defined( 'ABSPATH') or die( 'Unauthorized Access');
+
+
 function create_block_best_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_best_block_init' );
+
+
+
+
+/**
+ * Function that initializes the plugin.
+ */
+function norrenberger_get_stocks_data() { 
+	
+}
+
+
+/**
+ * Register a custom menu page to view the information queried.
+ */
+function norrenberger_register_custom_menu_page() {
+	add_menu_page(
+		__( 'Norrenberger API Settings', 'norrenberger' ),
+		'Norrenberger Stocks',
+		'manage_options',
+		'norrenberger',
+        'norrenberger_get_stocks_data',
+        'dashicons-testimonial',
+		16
+	);
+	
+    //adding a submenu called API Settings
+	add_submenu_page( 
+        'norrenberger', 
+        'api keys', 
+        'API Settings', 
+        'manage_options', 
+        'api_creds', 
+        'norrenberger_api_creds'
+    ); 
+
+}
