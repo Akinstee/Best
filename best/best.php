bd50d99dd26aab18e70258e647e4a7dac6e0440c
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-20 08:33:23 +0100

    setup namespace

--- best/best.php
+++ best/best.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Best
+ * Plugin Name:       Norrenberger
  * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.1
  * Requires PHP:      7.0
@@ -8,7 +8,7 @@
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       best
+ * Text Domain:       norrenberger
  *
  * @package           create-block
  */
