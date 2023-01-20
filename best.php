01198daf905a42a97d819c06a035dba2f1a538ba
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-20 11:10:11 +0000

    up a level

---
+++ best.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Plugin Name:       Norrenberger
+ * Description:       Example block scaffolded with Create Block tool.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       norrenberger
+ *
+ * @package           create-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+
+
+ // If this file is access directly, abort!!!
+defined( 'ABSPATH') or die( 'Unauthorized Access');
+
+
+function create_block_best_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_best_block_init' );
+
+
+
+
+/**
+ * Function that initializes the plugin.
+ */
+function norrenberger_get_stocks_data() { 
+	?>
+        <h1>Hello World<h1>
+    <?php
+}
+
+
+/**
+ * Register a custom menu page to view the information queried.
+ */
+function norrenberger_register_custom_menu_page() {
+	add_menu_page(
+		__( 'Norrenberger API Settings', 'norrenberger' ),
+		'Norrenberger Stocks',
+		'manage_options',
+		'norrenberger',
+        'norrenberger_get_stocks_data',
+        'dashicons-testimonial',
+		16
+	);
+	
+    // //adding a submenu called API Settings
+	// add_submenu_page( 
+    //     'norrenberger', 
+    //     'api keys', 
+    //     'API Settings', 
+    //     'manage_options', 
+    //     'api_creds', 
+    //     'norrenberger_api_creds'
+    // ); 
+
+}
+
+
+add_action( 'admin_menu', 'norrenberger_register_custom_menu_page' );
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2eb729438e1e9c7bb3ca');
