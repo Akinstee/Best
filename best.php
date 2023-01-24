c7a8528db15e45b1e728d941b57134ecf99a1c3a
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-24 07:44:02 +0000

    init display of fund-1-table to frontend using short code

--- best.php
+++ best.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Best
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Norrenberger Stocks Display
+ * Description:       This plugin helps display all the investment stocks information the on pages via shortcode.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            @Akinstee @kingdanie
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       best
@@ -31,6 +31,65 @@ function create_block_best_block_init() {
 }
 add_action( 'init', 'create_block_best_block_init' );
 
+
+add_shortcode('Best', 'norrenberger_fund_1');
+
+function norrenberger_fund_1 () {
+    ?>
+        <div class="display" >
+		<table id="fund-1-table" width="100%">
+  <thead>
+    <tr role="row">
+        <th>ID</th>
+        <th>SchemeId</th>
+        <th>ReportDate</th>
+        <th>Value</th>
+    </tr>
+
+  </thead>
+ 
+</table>
+</div>
+    <?php
+    // Write AJAX to show the infomation in the shortcode.
+    wp_enqueue_script( 'best-ajax-scripts', plugins_url( 'assets/js/best-ajax.js', __FILE__ ), ['jquery'], '0.1.0', true );
+    
+}
+
+
+// Create new endpoint to provide data.
+add_action( 'rest_api_init', 'best_rest_ajax_endpoint' );
+
+function best_rest_ajax_endpoint() {
+    register_rest_route(
+        'best',
+        'rest-ajax',
+        [
+            'methods'             => 'GET',
+            'permission_callback' => '__return_true',
+            'callback'            => 'best_rest_ajax_callback',
+        ]
+    );
+}
+
+
+// REST Endpoint information.
+function best_rest_ajax_callback() {
+
+    $url = 'http://ffpro.norrenberger.com/WebResourcesAPI/api/GetFundPrices?lastUpdatedDate=2022-10-21&fundId=73';
+    $id_token = '0yMfJUVun5dcWFp0Q4hAecWzNsveBlFAGSE8kds5ylJE_mPoc50oU2lQqNXN1c2jxc1Wyv02Gd4BxIJgLow_QhSaT8W1_7POsxpvDCsV59_evualPwPH0bHd5KgTzUuVpMqP7PuSymGfeoULmYu4rTMjs94LhnipEoUIECCmaFZLg9YTMwmhPuPNYJ5RTnBhEClMOg17LKn2q07Tqi970cGM-q-IUOodqGcVykd7wlh4jCqjnh83FLm_U-YJ4ySlqffL22rewahPBH8aHJw2Upkrq9OVEPtZegLar-b-jXZG9ctfDDsKahMip1hCpJVPthxVE3gl74v-IjXGn4x7cYdp7YFYaw_C-PoqO3yqmtKpRJHunU-h1RSGVqPlzBVpDiI153qGhXHGdv1jU8bUS523qMd3xXuqwkTjFN-oxHOtcPWZMGcImDn5fj6eIzb3vozV8JYZlfEK0okofxfCxoBdaAUXOspiPFuWxAi8cAEe7wUAqiTNidmVOa_Fw3_4';
+    $args = array(
+        'headers' => array(
+            'Authorization' => 'Bearer ' . $id_token,
+        )
+    );
+    $response = wp_remote_get( $url, $args );
+    $body     = wp_remote_retrieve_body( $response );
+    $Wot = $body;
+
+return $body;
+
+}
 
 
 
@@ -85,7 +144,6 @@ function best_get_stocks_data() {
     <?php
 }
 
-add_shortcode('Best', 'best_get_stocks_data');
 
 /**
  * Register a custom menu page to view the information queried.
@@ -115,3 +173,18 @@ function best_register_custom_menu_page() {
 
 
 add_action( 'admin_menu', 'best_register_custom_menu_page' );
+
+
+/**
+* Load custom CSS and JavaScript.
+*/
+add_action( 'wp_enqueue_scripts', 'wpdocs_my_enqueue_scripts' );
+function wpdocs_my_enqueue_scripts() : void {
+    // Enqueue my styles.
+    wp_enqueue_style( 'datatables-style', 'https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css' );
+     
+    // Enqueue my scripts.
+    wp_enqueue_script( 'datatables', 'https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js', ['jquery'], null, true );
+     wp_localize_script( 'datatables', 'datatablesajax', array('url' => admin_url('admin-ajax.php')) );
+
+}
