8f9f4b901f1aff0d7d3836738ca50da7721b5aa5
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-24 13:07:14 +0100

    modify ajax file

--- assets/js/best-ajax.js
+++ assets/js/best-ajax.js
@@ -1,11 +1,12 @@
 jQuery(document).ready(function($){
-
-	
-	$('#fund-1-table').DataTable({
+	$('#fund_table').DataTable({
 		"processing":true,
        //"serverSide":true,
-		ajax: {url: '/wp-json/best/rest-ajax', dataSrc:""},
-			columns: [
+		ajax: {
+            url: '/wp-json/best/rest-ajax', 
+            dataSrc:''
+        },
+        columns: [
                 { data: 'Id' },
                 { data: 'SchemeId' },
                 { data: 'ReportDate' },
