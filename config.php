44fc1f9f19f383838cf3e22591c687194e2a3560
kingdanie <daniedemola@gmail.com>
2023-01-28 23:40:27 +0100

    create config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'apiUrl' => "https://ffpro.norrenberger.com/WebResourcesAPI/",
+    'baseUrl' => 'https://ieianchorpensions.com.ng/',
+    'mobileUrl' => 'https://ffpro.norrenberger.com:6100/api',
+    'guestUserAccount' => 'GuestUser',
+    'guestUserPassword' => '#()JJJP@akadme1?}{'
+];
